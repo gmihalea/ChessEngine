82fabdea9ccacac625488a05466381a572f9bc08
cvpoienaru <cvpoienaru@gmail.com>
2014-03-09 19:59:15 +0200

    afsdf

--- Piece.java
+++ Piece.java
@@ -36,6 +36,30 @@ public abstract class Piece {
 		
 		return result;
 	}
+	
+	private static ArrayList<Square> checkDirection(Square square, int letter, int number, int opponentColor, int... opponents) {
+		int pieceType;
+		Square intermediate;
+		ArrayList<Square> result = new ArrayList<Square>();
+		
+		intermediate = square;
+		while(Board.isSquareValid(square.getLetter() + letter, square.getNumber() + number)) {
+			intermediate = Board.translate(square.getLetter() + letter, square.getNumber() + number);
+			if(intermediate.getPiece() != null) {
+				if(intermediate.getPiece().getColor() == opponentColor) {
+					pieceType = PieceType.getType(intermediate.getPiece());
+					for(int opponent : opponents) {
+						if(pieceType == opponent)
+							result.add(intermediate);
+						break;
+					}
+				}
+				else break;
+			}
+		}
+		
+		return result;
+	}
 
 	public static boolean canBeCaptured(Square square, int opponentColor) {
 		int number = square.getNumber();
@@ -43,6 +67,7 @@ public abstract class Piece {
 		int pieceType;
 		Square intermediate;
 		
+		//TODO: adauga pion
 		for(int i = -1; i <= 1; i++)
 			for(int j = -1; j <= 1; j++)
 				if(!(i == 0 && j == 0)) {
@@ -56,124 +81,17 @@ public abstract class Piece {
 							}
 							else break;
 						}
-					}	
+					}
+					
+					if(i != j) {
+						if(!Piece.checkDirection(square, i, j, opponentColor, PieceType.QUEEN, PieceType.ROOK).isEmpty())
+							return true;
+					}
+					else {
+						if(!Piece.checkDirection(square, i, j, opponentColor, PieceType.QUEEN, PieceType.BISHOP).isEmpty())
+							return true;
+					}
 				}
-		
-		for(int i = number; i < 8; i++) {
-			intermediate = Board.translate(letter, i + 1);
-			if(intermediate.getPiece() != null) {
-				if(intermediate.getPiece().getColor() == opponentColor) {
-					pieceType = PieceType.getType(intermediate.getPiece());
-					if(pieceType == PieceType.QUEEN || pieceType == PieceType.ROOK)
-						return true;
-					break;
-				}
-				else break;
-			}
-		}
-		
-		for(int i = number; i > 1; i--) {
-			intermediate = Board.translate(letter, i - 1);
-			if(intermediate.getPiece() != null) {
-				if(intermediate.getPiece().getColor() == opponentColor) {
-					pieceType = PieceType.getType(intermediate.getPiece());
-					if(pieceType == PieceType.QUEEN || pieceType == PieceType.ROOK)
-						return true;
-					break;
-				}
-				else break;
-			}
-		}
-		
-		for(int i = letter; i > Letter.A; i--) {
-			intermediate = Board.translate(i - 1, number);
-			if(intermediate.getPiece() != null) {
-				if(intermediate.getPiece().getColor() == opponentColor) {
-					pieceType = PieceType.getType(intermediate.getPiece());
-					if(pieceType == PieceType.QUEEN || pieceType == PieceType.ROOK)
-						return true;
-					break;
-				}
-				else break;
-			}
-		}
-		
-		for(int i = letter; i < Letter.H; i++) {
-			intermediate = Board.translate(i + 1, number);
-			if(intermediate.getPiece() != null) {
-				if(intermediate.getPiece().getColor() == opponentColor) {
-					pieceType = PieceType.getType(intermediate.getPiece());
-					if(pieceType == PieceType.QUEEN || pieceType == PieceType.ROOK)
-						return true;
-					break;
-				}
-				else break;
-			}
-		}
-	
-		for(int i = letter, j = number; i > Letter.A && j > 1; i--, j--) {
-			intermediate = Board.translate(i - 1, j - 1);
-			if(intermediate.getPiece() != null) {
-				if(intermediate.getPiece().getColor() == opponentColor) {
-					pieceType = PieceType.getType(intermediate.getPiece());
-					if(i == letter - 1 && j == number - 1
-							&& pieceType == PieceType.PAWN && opponentColor == PieceColor.WHITE)
-						return true;
-					if(pieceType == PieceType.QUEEN || pieceType == PieceType.BISHOP)
-						return true;
-					break;
-				}
-				else break;
-			}
-		}
-		
-		for(int i = letter, j = number; i > Letter.A && j < 8; i--, j++) {
-			intermediate = Board.translate(i - 1, j + 1);
-			if(intermediate.getPiece() != null) {
-				if(intermediate.getPiece().getColor() == opponentColor) {
-					pieceType = PieceType.getType(intermediate.getPiece());
-					if(i == letter - 1 && j == number + 1
-							&& (pieceType == PieceType.PAWN && opponentColor == PieceColor.BLACK))
-						return true;
-					if(pieceType == PieceType.QUEEN || pieceType == PieceType.BISHOP)
-						return true;
-					break;
-				}
-				else break;
-			}
-		}
-		
-		for(int i = letter, j = number; i < Letter.H && j < 8; i++, j++) {
-			intermediate = Board.translate(i + 1, j + 1);
-			if(intermediate.getPiece() != null) {
-				if(intermediate.getPiece().getColor() == opponentColor) {
-					pieceType = PieceType.getType(intermediate.getPiece());
-					if(i == letter + 1 && j == number + 1
-							&& (pieceType == PieceType.PAWN && opponentColor == PieceColor.BLACK))
-						return true;
-					if(pieceType == PieceType.QUEEN || pieceType == PieceType.BISHOP)
-						return true;
-					break;
-				}
-				else break;
-			}
-		}
-		
-		for(int i = letter, j = number; i < Letter.H && j > 1; i++, j--) {
-			intermediate = Board.translate(i + 1, j - 1);
-			if(intermediate.getPiece() != null) {
-				if(intermediate.getPiece().getColor() == opponentColor) {
-					pieceType = PieceType.getType(intermediate.getPiece());
-					if(i == letter + 1 && j == number - 1
-							&& (pieceType == PieceType.PAWN && opponentColor == PieceColor.WHITE))
-						return true;
-					if(pieceType == PieceType.QUEEN || pieceType == PieceType.BISHOP)
-						return true;
-					break;
-				}
-				else break;
-			}
-		}
 		
 		for(int i = -2; i <= 2; i++) {
 			for(int j = -2; j <= 2; j++) {
