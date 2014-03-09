cfa58b54ea50602518334e1754169c1d7812441b
gmihalea <geanina.mihalea@gmail.com>
2014-03-09 12:43:56 +0200

    King

--- King.java
+++ King.java
@@ -18,8 +18,23 @@ public class King extends Piece {
 
 	@Override
 	public ArrayList<Square> getValidSquares() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Square> result = new ArrayList<Square>();
+		int opponentColor = (this.getColor() == PieceColor.BLACK) ? PieceColor.WHITE : PieceColor.BLACK;
+		int row = this.getPosition().getNumber();
+		int column = this.getPosition().getLetter();
+		Square intermediate;
+		
+		for( int i = -1; i <= 1; i++)
+			for( int j = -1; j <= 1; j++)
+				if(!(i == 0 && j == 0)) {
+					if(Board.isSquareValid(column + i, row + j)){
+						intermediate = Board.get()[column + i][row + j];
+						if(intermediate.getPiece().getColor() == opponentColor || intermediate.getPiece() == null )
+							result.add(intermediate);
+					}	
+				}
+		
+		return result;
 	}
 
 	
