3882548c7bda50c59bd0dab19bd38a22977c5f8d
cvpoienaru <cvpoienaru@gmail.com>
2014-03-09 14:27:40 +0200

    Structure ready

--- Game.java
+++ Game.java
@@ -1,3 +1,33 @@
+
 public class Game {
 	
+	private static int mode = GameMode.DEFAULT;
+	private static long whiteClock;
+	private static long blackClock;
+	private static long myClock;
+	private static PieceSet mySet;
+	
+	public static void initialize() {
+		
+	}
+	
+	public static void makeMove(Move move) {
+		
+	}
+	
+	public static void setDefaultMode() {
+		
+	}
+	
+	public static void setForceMode() {
+		
+	}
+	
+	public static void setTurn(PieceColor color) {
+		
+	}
+	
+	public static void play() {
+		
+	}
 }
