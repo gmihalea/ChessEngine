62e5829de06843ae9e0b195177cb792c58160f89
cvpoienaru <cvpoienaru@gmail.com>
2014-03-09 16:29:58 +0200

    asfasdf

--- Game.java
+++ Game.java
@@ -8,11 +8,24 @@ public class Game {
 	private static PieceSet mySet;
 	
 	public static void initialize() {
-		
+		Board.initialize();
+		Game.whiteClock = Game.blackClock = Game.myClock = 0;
+		Game.mySet = Board.getBlackSet();
 	}
 	
 	public static void makeMove(Move move) {
+		if(move.getEndSquare().getPiece() != null) {
+			if(Game.mySet == Board.getWhiteSet())
+				Board.getBlackSet().capturePiece(move.getEndSquare().getPiece());
+			else
+				Board.getWhiteSet().capturePiece(move.getEndSquare().getPiece());
+		}
 		
+		move.getEndSquare().setPiece(move.getStartSquare().getPiece());
+		move.getStartSquare().setPiece(null);
+		move.getEndSquare().getPiece().setPosition(move.getEndSquare());
+		
+		Game.changeTurn();
 	}
 	
 	public static void setDefaultMode() {
@@ -23,8 +36,13 @@ public class Game {
 		
 	}
 	
-	public static void setTurn(PieceColor color) {
-		
+	public static void setTurn(int color) {
+		Game.mySet = (color == PieceColor.WHITE) ? Board.getBlackSet() : Board.getWhiteSet();
+		Game.myClock = (color == PieceColor.WHITE) ? Game.blackClock : Game.whiteClock;
+	}
+	
+	public static void changeTurn() {
+		Game.turn = (Game.turn == PieceColor.WHITE) ? PieceColor.BLACK : PieceColor.WHITE;
 	}
 	
 	public static void play() {
