0f43e6015477e6ba1c553dfca9b55d2b3ab74236
Bogdan Petcu <bogdanconstantinpetcu@gmail.com>
2014-03-29 16:11:51 +0200

    SpecialMoves.java

---
+++ SpecialMoves.java
@@ -0,0 +1,24 @@
+public class SpecialMoves {
+	
+	public static void pawnPromotion(Pawn pawn){
+		//apeleaza capture;
+		//pune o regina in loc
+	}
+	
+	public static void smallCastling(){
+		
+	}
+
+	public static void bigCastling() {
+		
+	}
+	
+	public static void enPassant(Pawn pawn){
+		
+	}
+	
+	public static void outOfCheck(){
+		//incearca sa mute intr-un captureFreeSquare
+		//altfel incearca sa blocheze cu alta piesa
+	}
+}
