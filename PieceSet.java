a2cda630671eba37e5255b0b0f62bf012d8834bf
Alin Valentin <alin_valentin.radulescu@cti.pub.ro>
2014-03-30 18:01:15 +0300

    melc

--- PieceSet.java
+++ PieceSet.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
 
 public class PieceSet {
-	
+
 	private static final int NO_OF_PIECES = 16;
-		
+
 	private ArrayList<Piece> available = null;
 	private ArrayList<Piece> captured = null;
 	private int color = 0;
-	
+
 	/**Initializes this piece set ( with NO pieces ) */
 	public PieceSet() {
 		this.available = new ArrayList<Piece>(PieceSet.NO_OF_PIECES); 
 		this.captured = new ArrayList<Piece>(PieceSet.NO_OF_PIECES);
 	}
-	
+
 	/**This set gets 16 pieces placed on the initial positions
 	 * <br> according to the specified color 
 	 * @param color PieceColor.WHITE or PieceColor.BLACK 
@@ -28,28 +28,33 @@ public class PieceSet {
 		this.addPiece(new Knight	(Board.translate(Letter.B, rowNumber), color)); 
 		this.addPiece(new Knight	(Board.translate(Letter.G, rowNumber), color)); 
 		this.addPiece(new Queen		(Board.translate(Letter.D, rowNumber), color));
-		
+
 		rowNumber = (color == PieceColor.WHITE) ? 2 : 7; 
 		for (int letter = Letter.A; letter <= Letter.H; letter++)
 			this.available.add(new Pawn(Board.translate(letter, rowNumber), color));
 		this.setColor(color);
 	}
-	
+
 	public void addPiece(Piece piece) {
 		this.available.add(piece);
 	}
-	
+
 	public ArrayList<Piece> getAvailablePieces() {
 		return this.available;
 	}
-	
+
 	public ArrayList<Piece> getCapturedPieces() {
 		return this.captured;
 	}
-	
 
-	
-	
+	public void capturePiece(Piece piece) {
+		for(int i = 0; i < this.available.size(); i++)
+			if(this.available.get(i).compareTo(piece) == 0) {
+				this.available.get(i).getPosition().setPiece(null);
+				this.captured.add(this.available.remove(i));
+				break;
+			}
+	}
 
 	public int getColor() {
 		return color;
